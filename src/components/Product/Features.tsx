5754201eb38be88590b9ca28268f493ca3d14186
janar <dev.janar@gmail.com>
2022-01-29 18:45:04 +0600

    fixed product search bug

--- src/components/Product/Features.tsx
+++ src/components/Product/Features.tsx
@@ -49,12 +49,12 @@ const Features = ({ features }) => {
               </H2>
             )}
             <Typography>
-              <div
-                className="product__keypoints-content"
-                dangerouslySetInnerHTML={{
-                  __html: feature?.content.replace('font-family:Titillium', ''),
-                }}
-              ></div>
+              {feature.content && feature.content.length &&
+                <div className="product__keypoints-content"
+                  dangerouslySetInnerHTML={{
+                    __html: feature?.content.replace('font-family:Titillium', ''),
+                  }}></div>
+              }
             </Typography>
           </div>
         </Card>
