2138d24e4b5759023b3ab1243e156b95d22ad1d6
devjanar <dev.janar@gmail.com>
2022-02-05 13:23:38 +0000

    create new branch and change api port

--- src/config/ApolloClient.ts
+++ src/config/ApolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://naubaun.herokuapp.com/graphql",
+  uri: "http://localhost:4500/graphql",
   cache: new InMemoryCache(),
 });
 
