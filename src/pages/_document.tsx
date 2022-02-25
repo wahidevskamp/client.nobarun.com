4ca01d96ccf6d21cf7bc3bbcd51d4a8b65eae4bf
janar <dev.janar@gmail.com>
2022-02-25 19:18:31 +0600

    pause fonts.gstatic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -47,12 +47,12 @@ export default class extends Document {
             }}
           />
           {/* <!-- End Google Tag Manager --> */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
+          {/*<link rel="preconnect" href="https://fonts.googleapis.com" />*/}
+          {/*<link*/}
+            {/*rel="preconnect"*/}
+            {/*href="https://fonts.gstatic.com"*/}
+            {/*crossOrigin=""*/}
+          {/*/>*/}
           <link
             href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;600&display=swap"
             rel="stylesheet"
