802317e94b734225ec95d5a24167c2a713a6a234
janar <dev.janar@gmail.com>
2022-03-06 20:03:30 +0600

    add api.biznian.com

--- src/config/ApolloClient.ts
+++ src/config/ApolloClient.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://naubaun.herokuapp.com/graphql",
+  // uri: "https://naubaun.herokuapp.com/graphql",
+  uri: "http://api.biznian.com/graphql",
   cache: new InMemoryCache(),
 });
 
--- src/config/GraphQLRequest.ts
+++ src/config/GraphQLRequest.ts
@@ -1,6 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 
-const Client = new GraphQLClient('https://naubaun.herokuapp.com/graphql', {
+// const Client = new GraphQLClient('https://naubaun.herokuapp.com/graphql', {
+const Client = new GraphQLClient('http://api.biznian.com/graphql', {
   headers: {},
 });
 
