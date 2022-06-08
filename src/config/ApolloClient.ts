fecaeea5000adff2405b9ba2c268581e1492499c
janar <dev.janar@gmail.com>
2022-06-08 21:49:04 +0600

    change api domain

--- src/config/ApolloClient.ts
+++ src/config/ApolloClient.ts
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   // uri: "https://naubaun.herokuapp.com/graphql",
-  uri: "https://api.biznian.com/graphql",
+  // uri: "https://api.biznian.com/graphql",
+  uri: "https://nobarun.xyz/graphql",
   cache: new InMemoryCache(),
 });
 
--- src/config/GraphQLRequest.ts
+++ src/config/GraphQLRequest.ts
@@ -1,7 +1,8 @@
 import { GraphQLClient } from 'graphql-request';
 
 // const Client = new GraphQLClient('https://naubaun.herokuapp.com/graphql', {
-const Client = new GraphQLClient('https://api.biznian.com/graphql', {
+// const Client = new GraphQLClient('https://api.biznian.com/graphql', {
+const Client = new GraphQLClient('https://nobarun.xyz/graphql', {
   headers: {},
 });
 
